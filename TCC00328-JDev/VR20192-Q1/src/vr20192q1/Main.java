dfc894b4d9ae8d198abc83a47c282fdcc6f910fc
Luiz Leme <lapaesleme@ic.uff.br>
2019-12-10 15:10:29 -0300

    ok

--- TCC00328-JDev/VR20192-Q1/src/vr20192q1/Main.java
+++ TCC00328-JDev/VR20192-Q1/src/vr20192q1/Main.java
@@ -1,8 +1,17 @@
 package vr20192q1;
 
+import vr20192q1.model.Participacao;
+import vr20192q1.model.Projeto;
+
 
 public abstract class Main {
     public static void main(String[] args) {
-
+        Projeto[] projetos;
+        for (Projeto p : projetos) {
+            for (Participacao r : p.getEquipe()) {
+                System.out.println(p.getNome() + "," + r.getFuncionario().getNome() + "'" + r.getInicio().toString() +
+                                   "," + r.getFim().toString());
+            }
+        }
     }
 }
